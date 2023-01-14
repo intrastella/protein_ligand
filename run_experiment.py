c3e583384b25b26f08b979f5edb158e48855d150
Stella <122531435+intrastella@users.noreply.github.com>
2023-01-14 17:26:07 +0100

    Update run_experiment.py

--- run_experiment.py
+++ run_experiment.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 
 """ 
-    ----------------------------- *** DESCRIPTION *** ----------------------------
+    ------------------------------- *** DESCRIPTION *** ------------------------------
     This module creates an instance of an experiment of a DL model.
     So far it consits of a mixture of WGan and Transformer model.
     You can specify checkpoints from prior trainings or chooses the best one.
-    ------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------
     
-    -------------------------------- *** DATA *** --------------------------------
+    ---------------------------------- *** DATA *** ----------------------------------
     The data types for training and evalutation : smiles and sdf
     You can choose to store the data in a sql database.
     When executing this script it will ask for your credentials from you sql databse.
-    ------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------
 
 
--------------------------------- *** LISCENCE *** --------------------------------
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
------------------------------------------------------------------------------------
+    -------------------------------- *** LISCENCE *** --------------------------------
+    This program is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option) any later
+    version.
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+    -----------------------------------------------------------------------------------
 """
 
 
