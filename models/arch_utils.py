d41280308bfe9e07fa07223c591dc1e3d4604a46
Stella <122531435+intrastella@users.noreply.github.com>
2023-01-14 12:30:49 +0100

    Update arch_utils.py

--- models/arch_utils.py
+++ models/arch_utils.py
@@ -24,7 +24,7 @@ class Architecture(Enum):
     GAN = "GAN"
 
 
-def get_model(model_name: Architecture, model_conf: Dict, ckpt_path: Union[List[Path], Path] = None):
+def get_model(model_name: Architecture, model_conf: Dict, ckpt_path: Union[List[Union[str, Path]], Union[str, Path]] = None):
     model = None
     if model_name == Architecture.GAN:
         if ckpt_path:
@@ -33,20 +33,19 @@ def get_model(model_name: Architecture, model_conf: Dict, ckpt_path: Union[List[
             if len(ckpt_path) != 2:
                 raise ValueError(f'1 checkpoint for genererator and 1 for discriminator is required. {len(ckpt_dir)} were given.')
             
-            model_dict = {}
+            model_conf = {}
             checkpoint = torch.load(ckpt_path[0])
-            model_dict[batch_size] = checkpoint['batch_size']
-            model_dict[training_steps] = checkpoint['training_steps']
-            model_dict[n_epoch] = checkpoint['n_epoch']
-            model_dict[n_critic] = checkpoint['n_critic']
-            model_dict[lr] = checkpoint['lr']
-            model_dict[b1] = checkpoint['b1']
-            model_dict[b2] = checkpoint['b2']
+            model_conf['batch_size'] = checkpoint['batch_size']
+            model_conf['training_steps'] = checkpoint['training_steps']
+            model_conf['n_epoch'] = checkpoint['n_epoch']
+            model_conf['n_critic'] = checkpoint['n_critic']
+            model_conf['lr'] = checkpoint['lr']
+            model_conf['b1'] = checkpoint['b1']
+            model_conf['b2'] = checkpoint['b2']
+            model_conf['ckpt_path'] = ckpt_path
             
-            model = GAN(**model_dict)
-            
-        else:
-            model = GAN(**model_conf)
+        model = GAN(**model_conf)
+        model.setup()
 
     elif model_name == Architecture.TRANSFORMER:
         model = MultiHead(**model_conf)
